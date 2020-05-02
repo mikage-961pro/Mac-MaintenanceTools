2cfd214e217771bc6c6e53d5e05c17b4e21b03a6
みかげ <50665049+mikage-961pro@users.noreply.github.com>
2020-05-02 18:56:26 +0900

    Add files via upload

---
+++ maint.sh
@@ -0,0 +1,53 @@
+echo "cleaning Xcode DerivedData"
+sudo rm -rf ~/Library/Developer/Xcode/DerivedData/*
+echo "clean Xcode DerivedData!"
+
+echo "cleaning  Xcode Archives!"
+sudo rm -rf ~/Library/Developer/Xcode/Archives/*
+echo "clean Xcode Archives!"
+
+echo "cleaning Library Caches"
+sudo rm -rf ~/Library/Caches/*
+echo "clean Library Caches!"
+
+echo "cleaning iOS logs"
+sudo rm -rf ~/Library/Logs/iOS\ Simulator
+echo "clean iOS logs"
+
+echo "全てのシミュレーターのキャッシュを削除します…"
+echo "全てのシミュレーターのキャッシュを削除しています…"
+sudo rm -rf ~/Library/Developer/Xcode/iOS\ DeviceSupport/*
+echo "clean simulator cache"
+
+echo "Macのメンテナンスを実施します"
+echo "Macのメモリー開放中…"
+sudo purge
+du -sx /
+echo "Macのメモリー開放完了"
+
+echo "アプリとOSのキャッシュのアップデート中…"
+sudo update_dyld_shared_cache -force 
+
+echo "ドライバキャッシュのクリア中…"
+sudo kextcache -system-caches
+
+echo "カーネルキャッシュの更新中…"
+sudo kextcache -system-prelinked-kernel
+
+echo "Mac OS メンテナンスモードの実行中…"
+sudo kextcache -system-prelinked-kernel
+
+echo "Mac OS アップデートの確認中…"
+sudo softwareupdate --list 
+echo "Mac OS アップデートチェック完了。"
+
+sudo softwareupdate --install --all
+echo "Mac OS アップデート完了。"
+
+echo "Mac OSセキュリティー設定のアップデート中"
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1
+/System/Library/CoreServices/MRT.app/Contents/MacOS/MRT -a
+defaults read /System/Library/CoreServices/MRT.app/Contents/Info.plist CFBundleShortVersionString
+softwareupdate -l --include-config-data
+
+echo "Mac OSセキュリティー設定のアップデート完了"
